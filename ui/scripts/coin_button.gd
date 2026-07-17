10900dd3c35122ba48006662bf363ca174643c72
VAastro19 <aetereth.novum@protonmail.com>
2026-07-17 22:59:33 +0200

    minor improvements and bug fixes

--- ui/scripts/coin_button.gd
+++ ui/scripts/coin_button.gd
@@ -1,46 +1,13 @@
 # coin_button.gd
-class_name CoinButton extends TextureButton
+class_name CoinButton extends Control
+
+@onready var button: TextureButton = $Button
 
 @export var coin_type: GameManager.CoinType
 @export var click_value: int = 1
 
-var normal_scale: float = 1.0
-var big_scale: float = 1.1
-var small_scale: float = 0.9
-
 func _ready() -> void:
-	mouse_entered.connect(_on_mouse_entered)
-	mouse_exited.connect(_on_mouse_exited)
-	pressed.connect(_on_pressed)
-	
-	match coin_type:
-		GameManager.CoinType.BLUE:
-			texture_normal = load("uid://c2pgc2wuco2a1")
-		GameManager.CoinType.GREEN:
-			texture_normal = load("uid://c0cxtfjfkdxag")
-		GameManager.CoinType.YELLOW:
-			texture_normal = load("uid://btpjfu7rn478t")
-		GameManager.CoinType.RED:
-			texture_normal = load("uid://dum12h5kdlrj4")
-		_:
-			texture_normal = load("uid://c2pgc2wuco2a1")
-	
-	texture_disabled = texture_normal
-	texture_pressed = texture_normal
-	texture_hover = texture_normal
-	texture_focused = texture_normal
+	button.pressed.connect(_on_button_pressed)
 
-func _on_mouse_entered() -> void:
-	scale.x = lerpf(scale.x, big_scale, 1.0)
-	scale.y = lerpf(scale.y, big_scale, 1.0)
-
-func _on_mouse_exited() -> void:
-	scale.x = lerpf(scale.x, normal_scale, 1.0)
-	scale.y = lerpf(scale.y, normal_scale, 1.0)
-
-func _on_pressed() -> void:
+func _on_button_pressed() -> void:
 	EventBus.OnUpdateCoin.emit(click_value, coin_type)
-	
-	var tween: Tween = get_tree().create_tween()
-	tween.tween_property(self, "scale", Vector2(small_scale, small_scale), 0.05)
-	tween.tween_property(self, "scale", Vector2(big_scale, big_scale), 0.05)
---
+++ ui/scripts/coin_button_visuals.gd
@@ -0,0 +1,43 @@
+# coin_button_visuals.gd
+extends TextureButton
+
+@onready var parent: CoinButton = get_parent()
+
+var normal_scale: float = 1.0
+var big_scale: float = 1.1
+var small_scale: float = 0.9
+
+func _ready() -> void:
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+	pressed.connect(_on_pressed)
+	
+	match parent.coin_type:
+		GameManager.CoinType.BLUE:
+			texture_normal = load("uid://c2pgc2wuco2a1")
+		GameManager.CoinType.GREEN:
+			texture_normal = load("uid://c0cxtfjfkdxag")
+		GameManager.CoinType.YELLOW:
+			texture_normal = load("uid://btpjfu7rn478t")
+		GameManager.CoinType.RED:
+			texture_normal = load("uid://dum12h5kdlrj4")
+		_:
+			texture_normal = load("uid://c2pgc2wuco2a1")
+	
+	texture_disabled = texture_normal
+	texture_pressed = texture_normal
+	texture_hover = texture_normal
+	texture_focused = texture_normal
+
+func _on_mouse_entered() -> void:
+	scale.x = lerpf(scale.x, big_scale, 1.0)
+	scale.y = lerpf(scale.y, big_scale, 1.0)
+
+func _on_mouse_exited() -> void:
+	scale.x = lerpf(scale.x, normal_scale, 1.0)
+	scale.y = lerpf(scale.y, normal_scale, 1.0)
+
+func _on_pressed() -> void:
+	var tween: Tween = get_tree().create_tween()
+	tween.tween_property(self, "scale", Vector2(small_scale, small_scale), 0.05)
+	tween.tween_property(self, "scale", Vector2(big_scale, big_scale), 0.05)
